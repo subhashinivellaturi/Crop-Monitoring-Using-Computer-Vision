e7caf68930061e186817453332cf98746693879d
subhashinivellaturi <22BQ1A47A6@vvit.net>
2025-04-02 18:46:15 +0530

    Update image_acquisition.py

--- src/image_acquisition.py
+++ src/image_acquisition.py
@@ -1,1 +1,20 @@
 # This file captures images from a camera or drone
+
+
+import cv2
+
+def capture_image(save_path="datasets/sample_images/crop_image.jpg"):
+    camera = cv2.VideoCapture(0)  # 0 for default webcam
+    ret, frame = camera.read()
+    
+    if ret:
+        cv2.imwrite(save_path, frame)
+        print(f"Image saved at {save_path}")
+    else:
+        print("Failed to capture image")
+    
+    camera.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    capture_image()
